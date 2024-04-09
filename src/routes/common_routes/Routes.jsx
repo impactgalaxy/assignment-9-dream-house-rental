8091a3234ef1889612bcc92ed3600a4062bbfd2f
impactgalaxy <traderglow@gmail.com>
2024-04-09 06:16:34 +0600

    login page before eid

--- src/routes/common_routes/Routes.jsx
+++ src/routes/common_routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../../pages/root/Root";
 import Home from "../../pages/home/Home";
 import NotFound from "../../pages/not_found/NotFound";
+import Login from "../../pages/login/Login";
+import SignUp from "../../pages/signUp/SignUp";
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +15,14 @@ export const router = createBrowserRouter([
                 path: "/",
                 element: <Home></Home>
 
+            },
+            {
+                path: "/user/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/user/signUp",
+                element: <SignUp></SignUp>
             }
         ]
     }
